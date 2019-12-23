abf9f0706a8464736de2e4c7e6bf873c50d510e5
Kresno <kresno.19@gmail.com>
2019-12-23 11:47:45 +0700

    page laporan

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -41,6 +41,10 @@
   <!-- Page level custom scripts -->
   <script src="<?php echo base_url('public/assets/js/demo/chart-area-demo.js'); ?>"></script>
   <script src="<?php echo base_url('public/assets/js/demo/chart-pie-demo.js'); ?>"></script>
+  <script src="<?php echo base_url('public/assets/vendor/datatables/jquery.dataTables.min.js'); ?>"></script>
+  <script src="<?php echo base_url('public/assets/vendor/datatables/dataTables.bootstrap4.min.js'); ?>"></script>
+
+  <script src="<?php echo base_url('public/assets/js/demo/datatables-demo.js'); ?>"></script>
 
 </body>
 
