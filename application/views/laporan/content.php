d831232c90df0b7c9482107f3c80ec89082eea18
Kresno <kresno.19@gmail.com>
2019-12-23 11:40:59 +0700

    page laporan

--- application/views/laporan/content.php
+++ application/views/laporan/content.php
@@ -516,3 +516,10 @@
 
     </div>
     <!-- End of Content Wrapper -->
+
+    <!-- Page level plugins -->
+    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/datatables-demo.js"></script>
