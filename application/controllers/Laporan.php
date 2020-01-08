427e917302ddf1de2bc80e007f180ed2cb722d14
Kresno <kresno.19@gmail.com>
2020-01-08 09:08:13 +0700

    edit all

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -21,7 +21,7 @@ class Laporan extends CI_Controller {
 	function __construct()
 	{
 		parent::__construct();
-		$this->load->model('M_laporan');
+		$this->load->model('m_laporan');
 	}
 	
 	public function index()
